af18b946440b28f5387f683a4965819e00ea77f0
Ryan Lafferty <rlafferty1@hotmail.com>
2018-03-29 01:13:35 -0400

    Added some contact chips

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types';
 import { Switch } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import Paper from 'material-ui/Paper';
+import EmailIcon from 'material-ui-icons/Email';
+import FaLinkedinSquare from 'react-icons/lib/fa/linkedin-square';
+import GoMarkGithub from 'react-icons/lib/go/mark-github';
 
-const styles = theme => ({});
+
+const styles = theme => ({
+    root: theme.mixins.gutters({
+        paddingTop: 16,
+        paddingBottom: 16,
+        marginTop: theme.spacing.unit * 3,
+      }),
+});
 
 class Contact extends React.Component {
   style = {
@@ -17,21 +30,39 @@ class Contact extends React.Component {
     },
   }
 
+  handleLinkedInClick = () => {
+    window.open('https://www.linkedin.com/in/ryan-lafferty-28a5a9bb/');
+  }
+
+  handleGithubClick = () => {
+    window.open('https://github.com/ryanlafferty');
+  }
+
   render() {
+    const { classes, theme } = this.props;
+
     return (
       <div style={this.style.root}>
-        <Typography variant="headline">
-          If you are interested in getting a hold of me consider contact through one of the methods below:
-        </Typography>
-        <br />
-        <Typography variant="headline">
-          Email: rlafferty1@hotmail.com
-        </Typography>
-        <Typography variant="headline" href="https://www.linkedin.com/in/ryan-lafferty-28a5a9bb/">
-          <a href="https://www.linkedin.com/in/ryan-lafferty-28a5a9bb/" style={this.style.link}>
-            LinkedIn
-          </a>
-        </Typography>
+        <Paper className={classes.root} elevation={4}>
+          <Typography variant="headline" component="h3">
+            If you are interested in getting a hold of me consider contact through one of the methods below:
+          </Typography>
+        </Paper>
+
+        <Chip
+            avatar={<Avatar> <EmailIcon /> </Avatar>}
+            label="rlafferty@hotmail.com"
+        />
+        <Chip
+            avatar={<Avatar> <FaLinkedinSquare /> </Avatar>}
+            label="LinkedIn"
+            onClick={this.handleLinkedInClick}
+        />
+        <Chip
+            avatar={<Avatar> <GoMarkGithub /> </Avatar>}
+            label="Github"
+            onClick={this.handleGithubClick}
+        />
       </div>
     );
   }
