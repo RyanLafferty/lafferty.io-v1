6c96dea4c0262a72329aa773a96f51bca7decef2
RyanLafferty <rlafferty1@hotmail.com>
2018-12-09 20:49:36 -0500

    Removing extra newline

--- src/data/DrawerData.js
+++ src/data/DrawerData.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-
 import HomeIcon from '@material-ui/icons/Home';
 import WorkIcon from '@material-ui/icons/Work';
 import CodeIcon from '@material-ui/icons/Code';
